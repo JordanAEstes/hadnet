931c59a64d69ad89f954a6f8ff138334cf0c9ad5
Winntana Beyene <winntanab@gmail.com>
2019-05-01 17:40:43 -0500

    added some source code to implement camera on isthisblackowned page

--- src/app/featured/featured.component.ts
+++ src/app/featured/featured.component.ts
@@ -2,11 +2,53 @@ import { Component, OnInit } from "@angular/core";
 import { RadSideDrawer } from "nativescript-ui-sidedrawer";
 import * as app from "tns-core-modules/application";
 
+import { ImageAsset } from "tns-core-modules/image-asset";
+import { takePicture, requestPermissions, isAvailable } from "nativescript-camera";
+
 @Component({
     selector: "Featured",
     moduleId: module.id,
     templateUrl: "./featured.component.html"
 })
+export class UsingCameraExampleComponent {
+    // >> camera-module-photo-code
+    public imageTaken: ImageAsset;
+    public saveToGallery: boolean = true;
+    public keepAspectRatio: boolean = true;
+    public width: number = 300;
+    public height: number = 300;
+
+    onTakePhoto() {
+        let options = {
+            width: this.width,
+            height: this.height,
+            keepAspectRatio: this.keepAspectRatio,
+            saveToGallery: this.saveToGallery
+        };
+
+        takePicture(options)
+            .then(imageAsset => {
+                this.imageTaken = imageAsset;
+                console.log("Size: " + imageAsset.options.width + "x" + imageAsset.options.height);
+            }).catch(err => {
+                console.log(err.message);
+            });
+    }
+    // << camera-module-photo-code
+
+    // >> camera-module-perm-code
+    onRequestPermissions() {
+        requestPermissions();
+    }
+    // << camera-module-perm-code
+
+    // >> camera-module-avai-code
+    onCheckForCamera() {
+        let isCameraAvailable = isAvailable();
+        console.log("Is camera hardware available: " + isCameraAvailable);
+    }
+    // << camera-module-avai-code
+}
 export class FeaturedComponent implements OnInit {
 
     constructor() {
@@ -21,4 +63,5 @@ export class FeaturedComponent implements OnInit {
         const sideDrawer = <RadSideDrawer>app.getRootView();
         sideDrawer.showDrawer();
     }
+    
 }
