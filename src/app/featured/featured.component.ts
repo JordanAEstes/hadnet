01dbe37fde437ff2b279c476439bf970bb5cbec0
Winntana Beyene <winntanab@gmail.com>
2019-05-02 13:42:10 -0500

    options set up on isThisBlackOwned page, camera not able to pop up yet

--- src/app/featured/featured.component.ts
+++ src/app/featured/featured.component.ts
@@ -2,55 +2,30 @@ import { Component, OnInit } from "@angular/core";
 import { RadSideDrawer } from "nativescript-ui-sidedrawer";
 import * as app from "tns-core-modules/application";
 
-import { ImageAsset } from "tns-core-modules/image-asset";
-import { takePicture, requestPermissions, isAvailable } from "nativescript-camera";
+//import { Component } from '@angular/core';
+import { takePicture, requestPermissions } from 'nativescript-camera';
+import { ImageAsset } from 'tns-core-modules/image-asset';
+
+
 
 @Component({
     selector: "Featured",
     moduleId: module.id,
     templateUrl: "./featured.component.html"
 })
-export class UsingCameraExampleComponent {
-    // >> camera-module-photo-code
-    public imageTaken: ImageAsset;
-    public saveToGallery: boolean = true;
-    public keepAspectRatio: boolean = true;
-    public width: number = 300;
-    public height: number = 300;
 
-    onTakePhoto() {
-        let options = {
-            width: this.width,
-            height: this.height,
-            keepAspectRatio: this.keepAspectRatio,
-            saveToGallery: this.saveToGallery
-        };
-
-        takePicture(options)
-            .then(imageAsset => {
-                this.imageTaken = imageAsset;
-                console.log("Size: " + imageAsset.options.width + "x" + imageAsset.options.height);
-            }).catch(err => {
-                console.log(err.message);
-            });
-    }
-    // << camera-module-photo-code
-
-    // >> camera-module-perm-code
-    onRequestPermissions() {
-        requestPermissions();
-    }
-    // << camera-module-perm-code
-
-    // >> camera-module-avai-code
-    onCheckForCamera() {
-        let isCameraAvailable = isAvailable();
-        console.log("Is camera hardware available: " + isCameraAvailable);
-    }
-    // << camera-module-avai-code
-}
 export class FeaturedComponent implements OnInit {
-
+    public saveToGallery: boolean = false;
+        public allowsEditing: boolean = false;
+        public keepAspectRatio: boolean = true;
+        public width: number = 320;
+        public height: number = 240;
+        public cameraImage: ImageAsset;
+        public actualWidth: number;
+        public actualHeight: number;
+        public scale: number = 1;
+        public labelText: string;
+    
     constructor() {
         // Use the component constructor to inject providers.
     }
@@ -63,5 +38,41 @@ export class FeaturedComponent implements OnInit {
         const sideDrawer = <RadSideDrawer>app.getRootView();
         sideDrawer.showDrawer();
     }
+    onTakePictureTap() : void {
+                requestPermissions().then(
+                    () => {
+                        takePicture({ width: this.width, height: this.height, keepAspectRatio: this.keepAspectRatio, saveToGallery: this.saveToGallery, allowsEditing: this.allowsEditing })
+                            .then((imageAsset: any) => {
+                                this.cameraImage = imageAsset;
+                                let that = this;
+                                imageAsset.getImageAsync(function (nativeImage, ex) {
+                                    if (ex instanceof Error) {
+                                        throw ex;
+                                    } else if (typeof ex === "string") {
+                                        throw new Error(ex);
+                                    }
+        
+                                    if (imageAsset.android) {
+                                        // get the current density of the screen (dpi) and divide it by the default one to get the scale
+                                        that.scale = nativeImage.getDensity() ;
+                                        that.actualWidth = nativeImage.getWidth();
+                                        that.actualHeight = nativeImage.getHeight();
+                                    } else {
+                                        that.scale = nativeImage.scale;
+                                        that.actualWidth = nativeImage.size.width * that.scale;
+                                        that.actualHeight = nativeImage.size.height * that.scale;
+                                    }
+                                    that.labelText = `Displayed Size: ${that.actualWidth}x${that.actualHeight} with scale ${that.scale}\n` +
+                                        `Image Size: ${Math.round(that.actualWidth / that.scale)}x${Math.round(that.actualHeight / that.scale)}`;
+        
+                                    console.log(`${that.labelText}`);
+                                });
+                            }, (error) => {
+                                console.log("Error: " + error);
+                            });
+                    },
+                    () => alert('permissions rejected')
+                );
+            }
     
 }
